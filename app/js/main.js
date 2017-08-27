b0128079f4941c9941d527603715dff876f0380d
Frank <dduction@gmail.com>
2017-08-27 14:16:13 -0700

    fix hero parallax and add body sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
     $(".button-collapse").sideNav({});
+
+    $(".hero__text--body").css({"padding-top": $(".hero__image--main").height() });
+
+    $('.parallax').parallax();
+
+    var iphoneBottom = $(".hero__bg--fullscreen").height() - $(".hero__container").height() - 100;
+    $("#hero__image--bottom").css({"min-height": $(".hero__image--main").height() - iphoneBottom });
+    
 });
